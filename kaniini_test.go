95d1fc35be8c25f1d4e9e9af7cc97318d554a924
Matthias Krull <m.krull@uninets.eu>
2019-12-25 15:14:12 +0100

    allow named exchanges

--- kaniini_test.go
+++ kaniini_test.go
@@ -28,7 +28,42 @@ func TestMain(m *testing.M) {
 		"amqp://guest:guest@localhost:5672",
 		"test-integration")
 
-	_ = consumer.Send([]byte("test message"))
+	_ = consumer.Send([]byte(expected))
+
+	timer := time.NewTimer(time.Second * 1)
+
+	select {
+	case msg := <-consumer.Receive():
+		msg.Ack()
+		consumer.Done() <- struct{}{}
+		fmt.Printf("Got: %s", string(msg.Body))
+		if string(msg.Body) != expected {
+			os.Exit(1)
+		}
+		os.Exit(m.Run())
+	case <-timer.C:
+		consumer.Done() <- struct{}{}
+		fmt.Println("Timeout while consuming messages.")
+		os.Exit(1)
+	}
+}
+
+func TestMainExchange(m *testing.M) {
+	expected := "test message kaniini"
+
+	exch1, _ := NewQueueOnExchange(
+		"amqp://guest:guest@localhost:5672",
+		"test-integration",
+		"kaniini1",
+		Direct)
+
+	exch2, _ := NewQueueOnExchange(
+		"amqp://guest:guest@localhost:5672",
+		"test-integration",
+		"kaniini2",
+		Direct)
+
+	_ = consumer.Send([]byte(expected))
 
 	timer := time.NewTimer(time.Second * 1)
 
